1d6203742a21d91e50a2f52f714d886e06868746
GrantWasil <dev@grantwasil.com>
2020-07-12 01:22:06 -0600

    feat: added remove button for elements

--- script.js
+++ script.js
@@ -1,31 +1,103 @@
 const profile = document.querySelector('.profile');
 const editButton = profile.querySelector('.btn-edit');
-const popup = document.querySelector('.popup');
-const closeButton = popup.querySelector('.popup__close');
+const newButton = profile.querySelector('.btn-new');
 const profileName = profile.querySelector('.profile__info-name');
 const profileInfo = profile.querySelector('.profile__info-title');
-const popupName = popup.querySelector('.popup__container-name');
-const popupAbout = popup.querySelector('.popup__container-about');
-const form = popup.querySelector('.popup__form');
+const elementsContainer = document.querySelector('.elements');
+const pageContainer = document.querySelector('.page');
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "https://code.s3.yandex.net/web-code/yosemite.jpg"
+    },
+    {
+        name: "Lake Louise",
+        link: "https://code.s3.yandex.net/web-code/lake-louise.jpg"
+    },
+    {
+        name: "Bald Mountains",
+        link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg"
+    },
+    {
+        name: "Latemar",
+        link: "https://code.s3.yandex.net/web-code/latemar.jpg"
+    },
+    {
+        name: "Vanois National Park",
+        link: "https://code.s3.yandex.net/web-code/vanois.jpg"
+    },
+    {
+        name: "Lago di Braies",
+        link: "https://code.s3.yandex.net/web-code/lago.jpg"
+    }
+];
 
-popupName.value = profileName.innerHTML;
-popupAbout.value = profileInfo.innerHTML;
-
-function popupToggle() {
-	popup.classList.toggle('popup_opened');
+const updateProfile = (popupName, popupAbout) => {
+	profileName.textContent = popupName; 
+	profileInfo.textContent = popupAbout;
 }
 
-function updateProfile(evt) {
-	evt.preventDefault();
-	profileName.textContent = popupName.value; 
-	profileInfo.textContent = popupAbout.value;
-	popupToggle();
+const addElement = (nameValue, linkValue) => {
+	const elementTemplate = document.querySelector('#element').content;
+	const newElement = elementTemplate.cloneNode(true);
+
+	newElement.querySelector('.element__image').src = linkValue;
+	newElement.querySelector('.element__info-name').textContent = nameValue;
+	const likeButton = newElement.querySelector('.element__info-like');
+	const trashButton = newElement.querySelector('.element__delete');
+
+	likeButton.addEventListener('click', (e) => {
+		e.target.classList.toggle('element__info-like_active');
+	})
+
+	trashButton.addEventListener('click', () => {
+		const targetElement = trashButton.closest('.element');
+		targetElement.remove();
+	})
+
+	elementsContainer.append(newElement);
 }
 
-editButton.addEventListener('click', popupToggle);
-closeButton.addEventListener('click', popupToggle);
-form.addEventListener('submit', updateProfile);
+const addPopup = (titleValue, namePlaceholder, aboutPlaceholder, saveFunction, defaultName="", defaultAbout="") => {
+	const popupTemplate = document.querySelector('#popup').content;
+	const newPopup = popupTemplate.cloneNode(true);
+	const name = newPopup.querySelector('.popup__container-name');
+	const about = newPopup.querySelector('.popup__container-about');
 
+	newPopup.querySelector('.popup__container-title').textContent = titleValue;
+	name.placeholder = namePlaceholder;
+	name.value = defaultName;
+	about.placeholder = aboutPlaceholder;
+	about.value = defaultAbout;
+	const closeButton = newPopup.querySelector('.popup__close');
+	const saveButton = newPopup.querySelector('.popup__container-save');
 
+	closeButton.addEventListener('click', () => {
+		const targetPopup = closeButton.closest('.popup');
+		targetPopup.remove();
+	})
 
+	saveButton.addEventListener('click', (e) => {
+		const targetPopup = saveButton.closest('.popup');
+		const popupName = targetPopup.querySelector('.popup__container-name');
+		const popupAbout = targetPopup.querySelector('.popup__container-about');
+		e.preventDefault();
+		saveFunction(popupName.value, popupAbout.value);
+		targetPopup.remove();
+	})
+
+	pageContainer.append(newPopup);
+}
+
+editButton.addEventListener('click', () => {
+	addPopup('Edit Profile', 'Name', 'About Me', updateProfile, profileName.textContent, profileInfo.textContent);
+});
+
+newButton.addEventListener('click', () => {
+	addPopup('New Place', 'Title', 'Image URL', addElement);
+})
+
+initialCards.forEach((card) => {
+	addElement(card.name, card.link);
+})
 
