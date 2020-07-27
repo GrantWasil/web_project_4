22588e199f6c5ebc344eff0a9a1b138cce7de6d8
GrantWasil <dev@grantwasil.com>
2020-07-27 00:09:22 -0600

    feat: added form validation into both forms

--- script.js
+++ script.js
@@ -99,6 +99,65 @@ const togglePopup = (popup) => {
 	popup.classList.toggle('popup_opened');
 }
 
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add("popup__input_type_error");
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add("popup__input-error_active");
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove("popup__input_type_error");
+  errorElement.classList.remove("popup__input-error_active");
+  errorElement.textContent = "";
+};
+
+const checkInputValidity = (formElement, inputElement) => {
+	if (!inputElement.validity.valid) {
+		showInputError(formElement, inputElement, inputElement.validationMessage);
+	} else {
+		hideInputError(formElement, inputElement);
+	}
+}
+
+const hasInvalidInput = (inputList) => {
+	return inputList.some(inputElement => !inputElement.validity.valid) 
+}
+
+const toggleButtonState = (inputList, buttonElement) => {
+	if (hasInvalidInput(inputList)) {
+		buttonElement.classList.add("popup__container-save_inactive");
+	} else {
+		buttonElement.classList.remove("popup__container-save_inactive");
+	}
+}
+
+const setEventListeners = (formElement) => {
+	const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+	const buttonElement = formElement.querySelector('.popup__container-save');
+
+	toggleButtonState(inputList, buttonElement);
+	inputList.forEach((inputElement) => {
+		inputElement.addEventListener('input', () => {
+			checkInputValidity(formElement, inputElement);
+			toggleButtonState(inputList, buttonElement);
+		});
+	});
+}
+
+const enableValidation = () => {
+	const formList = Array.from(document.querySelectorAll('.popup__form'));
+	formList.forEach((formElement) => {
+		formElement.addEventListener('submit', evt => evt.preventDefault());
+		const fieldsetList = Array.from(formElement.querySelectorAll(".popup__field"));
+
+		fieldsetList.forEach((fieldset) => {
+			setEventListeners(fieldset);
+		});
+	});
+}
+
 editButton.addEventListener('click', () => togglePopup(editPopup));
 editClose.addEventListener('click', () => togglePopup(editPopup));
 editForm.addEventListener('submit', (e) => {
@@ -117,5 +176,5 @@ newForm.addEventListener('submit', (e) => {
 
 pictureClose.addEventListener('click', () => picture.classList.toggle('picture_active'));
 
-
+enableValidation();
 
