1b56b9b093bee861a2bde0f8141028921c3c3408
GrantWasil <dev@grantwasil.com>
2020-08-06 17:32:02 -0600

    fix: refactored js into two files and fixed profile sizing

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,16 @@
+/**
+Okay, some pretty big changes here. First off, I want to apologise for not originally accepting your feedback.
+For some reason, I thought that you were wanting us to declare classes and export them, and I was just confused.
+
+I super appreciate your help, and I think that not only is my validation.js file what you were looking for, but 
+it's also helping me become a stronger dev.
+
+Again, I'm very sorry for the confusion. 
+
+Grant
+**/
+
+
 const profile = document.querySelector('.profile');
 const editButton = profile.querySelector('.btn-edit');
 const newButton = profile.querySelector('.btn-new');
@@ -23,7 +36,6 @@ const pictureClose = document.querySelector('.picture__container-close');
 const pageContainer = document.querySelector('.page');
 const elementTemplate = document.querySelector('#element').content;
 
-// I don't think we've learned how to pull out initialCards into a seperate file yet. All of the work we've done so far is with one JS file.
 const initialCards = [
     {
         name: "Yosemite Valley",
@@ -52,8 +64,12 @@ const initialCards = [
 ];
 
 const togglePopup = (popup) => {
-	popup.classList.toggle('popup_opened');
-	document.removeEventListener('keydown', handleEscape);
+    popup.classList.toggle('popup_opened'); 
+    if (popup.classList.contains('popup_opened')) {
+        document.addEventListener('keydown', handleEscape);
+    } else {
+        document.removeEventListener('keydown', handleEscape);
+    }
 }
 
 const updateProfile = (evt) => {
@@ -99,80 +115,6 @@ const editPicture = (nameValue, linkValue) => {
 	pictureUrl.alt = `Photo of ${nameValue}`;
 }
 
-for(const {name, link} of initialCards){
-	addElement(name, link);
-}
-
-// I'm confused about why you're asking me to move things to validate.js. It isn't mentioned in the Project 6 description nor the checklist. 
-// We haven't learned how to do multiple file JS yet. I'm familier with exporting functions so I can do it how I've been taught before, 
-// But I think you're asking me of requirements for a future project. 
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-
-  // You talked about passing the classList to the function, but we didnt' learn that either in our lessons. I just wanted to check to ensure that 
-  // I'm supposed to do that for project 6. If I am, I'll get it fixed tonight! Thank you again for your help!
-  inputElement.classList.add("popup__input_type_error");
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add("popup__input-error_active");
-};
-
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
-  inputElement.classList.remove("popup__input_type_error");
-  errorElement.classList.remove("popup__input-error_active");
-  errorElement.textContent = "";
-};
-
-const checkInputValidity = (formElement, inputElement) => {
-	if (!inputElement.validity.valid) {
-		showInputError(formElement, inputElement, inputElement.validationMessage);
-	} else {
-		hideInputError(formElement, inputElement);
-	}
-}
-
-const hasInvalidInput = (inputList) => {
-	return inputList.some(inputElement => !inputElement.validity.valid) 
-}
-
-const toggleButtonState = (inputList, buttonElement) => {
-	if (hasInvalidInput(inputList)) {
-		buttonElement.classList.add("popup__container-save_inactive");
-		buttonElement.disabled = true;
-	} else {
-		buttonElement.classList.remove("popup__container-save_inactive");
-		buttonElement.disabled = false;
-	}
-}
-
-const setEventListeners = (formElement) => {
-	const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
-	const buttonElement = formElement.querySelector('.popup__container-save');
-
-	toggleButtonState(inputList, buttonElement);
-	inputList.forEach((inputElement) => {
-		inputElement.addEventListener('input', () => {
-			checkInputValidity(formElement, inputElement);
-			toggleButtonState(inputList, buttonElement);
-		});
-	});
-}
-
-const enableValidation = () => {
-	const formList = Array.from(document.querySelectorAll('.popup__form'));
-	formList.forEach((formElement) => {
-		formElement.addEventListener('submit', evt => evt.preventDefault());
-		const fieldsetList = Array.from(formElement.querySelectorAll(".popup__field"));
-
-		fieldsetList.forEach((fieldset) => {
-			setEventListeners(fieldset);
-		});
-	});
-}
-
-
-
-// Thanks for your feedback regarding my "Escape" function. I've completly refactored this section and now my handlePopup function is much cleaner
 const handleEscape = (evt) => {
 	if (evt.key === "Escape") {
 		const currentPopup = document.querySelector('.popup_opened')
@@ -180,11 +122,16 @@ const handleEscape = (evt) => {
 	}
 };
 
+// I saw that you said that I no longer need this function, but this is how I currently prepare all of my buttons be clicked. I did 
+// remove the addEventListener('keydown', handleEscape) though because I implemented it above as you suggested
 const handlePopup = (buttonElement, popupElement) => {
 	buttonElement.addEventListener('click', () => {
 		togglePopup(popupElement);
-		document.addEventListener('keydown', handleEscape);
 	})
+}
+
+for(const {name, link} of initialCards){
+	addElement(name, link);
 }
 
 handlePopup(editButton, editPopup);
@@ -199,15 +146,13 @@ handlePopup(newClose, newPopup);
 handlePopup(newOverlay, newPopup);
 newForm.addEventListener('submit', (e) => {
 	const newNameValue = newName.value;
-	// Facepalm... I forgot a period before my class selector! Thansk for pointing that out.
 	const newLinkValue = newAbout.value;
-	e.preventDefault();
 	addElement(newNameValue, newLinkValue);
-	newPopup.classList.toggle('popup_opened');
+	togglePopup(newPopup);
 });
 
 
 pictureClose.addEventListener('click', () => picture.classList.toggle('picture_active'));
 
-enableValidation();
+
 
